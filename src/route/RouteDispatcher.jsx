e851216066f3469a79044b42c00141feeedbda66
chzhang981 <edwardzhangcongkai@gmail.com>
2023-11-19 17:46:24 -0600

    temp post page

--- src/route/RouteDispatcher.jsx
+++ src/route/RouteDispatcher.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route,  } from "react-router-dom";
 import LoginPage from "../components/LoginPage/LoginPage";
 import NeedTutorPage from "../components/NeedTutorPage/NeedTutorPage";
+import PostPage from "../components/PostPage/PostPage";
 
 const RouteDispatcher = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        {/* <Route path="/" element={<PostPage />} /> */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/need-tutor-page" element={<NeedTutorPage />} />
       </Routes>
