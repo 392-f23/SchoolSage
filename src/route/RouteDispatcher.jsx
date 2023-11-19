c8f7e099d1cf54246af6014558cd210ff10bc91d
Godfrey Oji <godfreyoji@dhcp-10-105-74-231.wireless.northwestern.private>
2023-11-19 10:47:15 -0600

    user profile for tutor and needs

--- src/route/RouteDispatcher.jsx
+++ src/route/RouteDispatcher.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route,  } from "react-router-dom";
 import LoginPage from "../components/LoginPage/LoginPage";
 import NeedTutorPage from "../components/NeedTutorPage/NeedTutorPage";
+import UserProfilePostTutor from "../components/UserProfile/UserProfilePostTutor";
+import UserProfilePostNeed from "../components/UserProfile/UserProfilePostNeed";
 
 const RouteDispatcher = () => {
   return (
@@ -9,6 +11,8 @@ const RouteDispatcher = () => {
         <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/need-tutor-page" element={<NeedTutorPage />} />
+        <Route path="/user-profileTutor" element={<UserProfilePostTutor />} />
+        <Route path="/user-profileNeed" element={<UserProfilePostNeed />} />
       </Routes>
     </BrowserRouter>
   );
