26c831c0a112e30052fdaf306ac9f547a378b9ac
DiffatElicka <37790161+DiffatElicka@users.noreply.github.com>
2023-11-19 20:21:18 -0600

    Tutor Page Basic (#5)
    Co-authored-by: HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>

--- src/route/RouteDispatcher.jsx
+++ src/route/RouteDispatcher.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route,  } from "react-router-dom";
 import LoginPage from "../components/LoginPage/LoginPage";
 import NeedTutorPage from "../components/NeedTutorPage/NeedTutorPage";
+import TutorPage from "../components/TutorPage/TutorPage";
 import PostPage from "../components/PostPage/PostPage";
 
 const RouteDispatcher = () => {
@@ -11,6 +12,7 @@ const RouteDispatcher = () => {
         {/* <Route path="/" element={<PostPage />} /> */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/need-tutor-page" element={<NeedTutorPage />} />
+        <Route path="/tutor" element={<TutorPage/>} />
       </Routes>
     </BrowserRouter>
   );
