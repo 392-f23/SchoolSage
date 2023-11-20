47d1a153c3d982a3d1c1d5a6e74e31e333f871d5
adrianhoffer35@gmail.com <adrianhoffer35@gmail.com>
2023-11-19 19:39:30 -0600

    added skeleton for need tutor

--- src/components/NeedTutorPage/NeedTutorPage.jsx
+++ src/components/NeedTutorPage/NeedTutorPage.jsx
@@ -1,6 +1,15 @@
 import "./NeedTutorPage.less";
 import { firebaseSignOut } from "../../utilities/firebaseUtils";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import postings from "./postings.json";
+import ChatIcon from '@mui/icons-material/Chat';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const NeedTutorPage = () => {
   const navigate = useNavigate();
@@ -9,10 +18,40 @@ const NeedTutorPage = () => {
     firebaseSignOut();
     navigate("/login");
   };
-
+  const handleAddPosting = () => {
+    console.log("post")
+  };
   return (
     <div className="need-tutor-page">
-      <button onClick={handleUserSignOut}>Test Signout</button>
+      <div className="header">
+        <Button style={{ backgroundColor: '#ceb8ef', color: "black", width:"25vw", margin:"5px"}} onClick={handleUserSignOut} endIcon={<LogoutIcon />}>Signout</Button>
+        <div className="title">Tutor Requests</div>
+      </div>
+      <div className="posting-box">
+      {Object.values(postings).map((user, index) => (
+          <Card style={{margin:"10px", }} key={index} className="user-card">
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {user.name}
+              </Typography>
+              <Typography color="text.secondary">
+                {user.level}
+              </Typography>
+              <Typography variant="body2">
+                {user.location}
+              </Typography>
+              <Typography variant="body2">
+                {user.Time}
+              </Typography>
+              <Typography variant="body2">
+                {user.Description}
+              </Typography>
+              <Button variant="contained" endIcon={<ChatIcon />}>Contact </Button>
+            </CardContent>
+          </Card>
+        ))}
+        </div>
+      <Button style={{ backgroundColor: '#ceb8ef', color: "black", width:"25vw", margin:"0 auto", marginTop: "10px"}} onClick={handleAddPosting} variant="contained" endIcon={<AddIcon />}>Post Request</Button>
     </div>
   );
 };
