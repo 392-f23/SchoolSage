cd88f6abfdbe2a3494bd548fb562f163d2100645
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-11-29 11:50:47 -0600

    Hongda/headbar (#6)
    * have basic header finished
    
    * header finished
    
    * add routing

---
+++ src/components/SchoolSageHeader/SchoolSageHeader.jsx
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import SearchBar from "material-ui-search-bar";
+import Button from "@mui/material/Button";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import "./SchoolSageHeader.less";
+
+const SchoolSageHeader = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [openNavigation, setOpenNavigation] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isNeedTutorPage = location.pathname === "/need-tutor-page";
+  const isTutorPage = location.pathname === "/tutor-page";
+  const isUserProfile = location.pathname === "/user-profile";
+
+  const navigateToNeedTutor = () => {
+    if (!isNeedTutorPage) {
+      navigate("/need-tutor-page");
+    }
+  };
+
+  const navigateToTutor = () => {
+    if (!isTutorPage) {
+      navigate("/tutor-page");
+    }
+  };
+
+  const navigateToUserProfile = () => {
+    if (!isUserProfile) {
+      navigate("/user-profile");
+    }
+  };
+
+  return (
+    <>
+      <div className="header">
+        {/* icon */}
+        <div className="header-icon-container">
+          <img
+            className="header-icon"
+            src="https://raw.githubusercontent.com/Hongda-OSU/PicGo-2.3.1/master/imgstudying%20(2).png"
+          />
+        </div>
+        {/* title */}
+        <div className="header-title-container">
+          <div className="upper-section">
+            <span className="header-title-text">S</span>
+            <span className="header-title-text">c</span>
+            <span className="header-title-text">h</span>
+            <span className="header-title-text">o</span>
+            <span className="header-title-text">o</span>
+            <span className="header-title-text">l</span>
+            <span className="header-title-text active">S</span>
+            <span className="header-title-text active">a</span>
+            <span className="header-title-text active">g</span>
+            <span className="header-title-text active">e</span>
+          </div>
+          <div className="lower-section">
+            <span className="header-slogan-text">
+              Knowledge at Your Fingertips!
+            </span>
+          </div>
+        </div>
+        {/* search */}
+        <div className="header-search-container">
+          <SearchBar
+            className="header-search"
+            placeholder="Search Your Tutor"
+            value={searchValue}
+            onChange={(searchValue) => setSearchValue(searchValue)}
+            onRequestSearch={() => {}}
+          />
+        </div>
+        {/* user interaction */}
+        <div className="header-user-interaction-container">
+          <div className="header-user-interaction-navigate">
+            <Button
+              variant="contained"
+              className="navigate-button"
+              onClick={() => setOpenNavigation(true)}
+              startIcon={<HomeIcon />}
+            >
+              Navigate
+            </Button>
+          </div>
+          <div className="header-user-interaction-profile">
+            <Button
+              variant="contained"
+              className="profile-button"
+              onClick={navigateToUserProfile}
+              startIcon={<PersonIcon />}
+            >
+              Profile
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {openNavigation && (
+        <div className="navigation">
+          <div className="navigation-container">
+            <div
+              className="navigation-need-tutor"
+              onClick={navigateToNeedTutor}
+            >
+              <img
+                src="https://raw.githubusercontent.com/Hongda-OSU/PicGo-2.3.1/master/img112312312313.png"
+                className="navigation-need-tutor-icon"
+              />
+              <span className="navigation-need-tutor-text">I Need Tutor</span>
+            </div>
+            <div className="navigation-post-tutor" onClick={navigateToTutor}>
+              <img
+                src="https://raw.githubusercontent.com/Hongda-OSU/PicGo-2.3.1/master/imgstudying.png"
+                className="navigation-post-tutor-icon"
+              />
+              <span className="navigation-post-tutor-text">I Post Tutor</span>
+            </div>
+          </div>
+          <button
+            className="navigation-close-button"
+            onClick={() => setOpenNavigation(false)}
+          >
+            X
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default SchoolSageHeader;
--- src/route/RouteDispatcher.jsx
+++ src/route/RouteDispatcher.jsx
@@ -1,22 +1,31 @@
-import { BrowserRouter, Routes, Route,  } from "react-router-dom";
+import { BrowserRouter, Routes, Route,  Navigate} from "react-router-dom";
+import { useAuthState } from "../utilities/firebaseUtils";
 import LoginPage from "../components/LoginPage/LoginPage";
 import NeedTutorPage from "../components/NeedTutorPage/NeedTutorPage";
-import UserProfilePostTutor from "../components/UserProfile/UserProfilePostTutor";
-import UserProfilePostNeed from "../components/UserProfile/UserProfilePostNeed";
 import TutorPage from "../components/TutorPage/TutorPage";
 import PostPage from "../components/PostPage/PostPage";
 
 const RouteDispatcher = () => {
+  const [user] = useAuthState();
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+      <Route
+          path="/"
+          element={
+            user ? (
+              <Navigate replace to="/need-tutor-page" />
+            ) : (
+              <Navigate replace to="/login" />
+            )
+          }
+        />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/need-tutor-page" element={<NeedTutorPage />} />
         <Route path="/tutor-page" element={<TutorPage/>} />
         <Route path="/post-tutor-page" element={<PostPage />} />
-        <Route path="/user-profileTutor" element={<UserProfilePostTutor />} />
-        <Route path="/user-profileNeed" element={<UserProfilePostNeed />} />
+        <Route path="/user-profile" element={<PostPage />} />
       </Routes>
     </BrowserRouter>
   );
