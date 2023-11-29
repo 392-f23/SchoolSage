f01f05cb90cdd271faf470f878ef56f307e835b3
Hongda <67817916+Hongda-OSU@users.noreply.github.com>
2023-11-29 11:42:35 -0600

    header finished

--- src/components/SchoolSageHeader/SchoolSageHeader.jsx
+++ src/components/SchoolSageHeader/SchoolSageHeader.jsx
@@ -1,14 +1,25 @@
-import "./SchoolSageHeader.less"
+import { useState } from "react";
+import SearchBar from "material-ui-search-bar";
+import Button from "@mui/material/Button";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import "./SchoolSageHeader.less";
 
 const SchoolSageHeader = () => {
-    return (
-        <div className="header">
+  const [searchValue, setSearchValue] = useState("");
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  return (
+    <>
+      <div className="header">
+        {/* icon */}
         <div className="header-icon-container">
           <img
             className="header-icon"
             src="https://raw.githubusercontent.com/Hongda-OSU/PicGo-2.3.1/master/imgstudying%20(2).png"
           />
         </div>
+        {/* title */}
         <div className="header-title-container">
           <div className="upper-section">
             <span className="header-title-text">S</span>
@@ -23,15 +34,73 @@ const SchoolSageHeader = () => {
             <span className="header-title-text active">e</span>
           </div>
           <div className="lower-section">
-            <span className="header-slogan-text">Knowledge at Your Fingertips!</span>
+            <span className="header-slogan-text">
+              Knowledge at Your Fingertips!
+            </span>
           </div>
         </div>
-        <div className="header-navigation-container">
-          
+        {/* search */}
+        <div className="header-search-container">
+          <SearchBar
+            className="header-search"
+            placeholder="Search Your Tutor"
+            value={searchValue}
+            onChange={(searchValue) => setSearchValue(searchValue)}
+            onRequestSearch={() => {}}
+          />
         </div>
-        <div className="header-user-interaction-container"></div>
+        {/* user interaction */}
+        <div className="header-user-interaction-container">
+          <div className="header-user-interaction-navigate">
+            <Button
+              variant="contained"
+              className="navigate-button"
+              onClick={() => setOpenNavigation(true)}
+              startIcon={<HomeIcon />}
+            >
+              Navigate
+            </Button>
+          </div>
+          <div className="header-user-interaction-profile">
+            <Button
+              variant="contained"
+              className="profile-button"
+              startIcon={<PersonIcon />}
+            >
+              Profile
+            </Button>
+          </div>
+        </div>
       </div>
-    );
-}
 
-export default SchoolSageHeader
+      {openNavigation && (
+        <div className="navigation">
+          <div className="navigation-container">
+            <div className="navigation-need-tutor">
+              <img
+                src="https://raw.githubusercontent.com/Hongda-OSU/PicGo-2.3.1/master/img112312312313.png"
+                className="navigation-need-tutor-icon"
+              />
+              <span className="navigation-need-tutor-text">I Need Tutor</span>
+            </div>
+            <div className="navigation-post-tutor">
+              <img
+                src="https://raw.githubusercontent.com/Hongda-OSU/PicGo-2.3.1/master/imgstudying.png"
+                className="navigation-post-tutor-icon"
+              />
+              <span className="navigation-post-tutor-text">I Post Tutor</span>
+            </div>
+          </div>
+          <button
+            className="navigation-close-button"
+            onClick={() => setOpenNavigation(false)}
+          >
+            X
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default SchoolSageHeader;
