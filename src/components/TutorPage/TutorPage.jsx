d7a7ce24550ec26ed727cecb80ba41ea8ba75b67
Hongda <67817916+Hongda-OSU@users.noreply.github.com>
2023-11-30 21:21:17 -0600

    update

--- src/components/TutorPage/TutorPage.jsx
+++ src/components/TutorPage/TutorPage.jsx
@@ -1,37 +1,46 @@
-import './Tutorpage.less'
+import "./Tutorpage.less";
 import TutorCard from "../TutorCard/TutorCard";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import SchoolSageHeader from "../SchoolSageHeader/SchoolSageHeader";
-import { useDbData } from "../../utilities/firebaseUtils"
-import AddIcon from '@mui/icons-material/Add';
-import Fab from '@mui/material/Fab';
-
-const navigateToPostTutor = () => {
-  navigate("/post-tutor-page")
-};
+import { useDbData } from "../../utilities/firebaseUtils";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
 
 const TutorPage = () => {
-    const [data, error] = useDbData('/Tutor-Page');
-    if (error) return <div className="tutor-page">
-    <SchoolSageHeader />
+  const navigate = useNavigate();
 
-    <h1>Error loading course data: {`${error}`}</h1>
-    </div>;
-    if (data === undefined) return <div className="tutor-page">
-    <SchoolSageHeader />
+  const navigateToPostTutor = () => {
+    navigate("/post-tutor-page");
+  };
+  
+  const [data, error] = useDbData("/Tutor-Page");
+  if (error)
+    return (
+      <div className="tutor-page">
+        <SchoolSageHeader />
 
-    <h1>Loading data...</h1>
-    </div>;
-    // if (!data) return <h1>No course data found</h1>;
+        <h1>Error loading course data: {`${error}`}</h1>
+      </div>
+    );
+  if (data === undefined)
+    return (
+      <div className="tutor-page">
+        <SchoolSageHeader />
 
-    return <div className="tutor-page">
+        <h1>Loading data...</h1>
+      </div>
+    );
+  // if (!data) return <h1>No course data found</h1>;
+
+  return (
+    <div className="tutor-page">
       <SchoolSageHeader />
 
       <div className="posting-box">
-      {Object.entries(data).map(( itemInfo, id ) => {
-        return <TutorCard key={itemInfo[0]} itemInfo={itemInfo} />}
-        
-      )}
+        {Object.entries(data).map((itemInfo, id) => {
+          return <TutorCard key={itemInfo[0]} itemInfo={itemInfo} />;
+        })}
       </div>
 
       <Fab
@@ -42,14 +51,13 @@ const TutorPage = () => {
           marginTop: "10px",
           marginBottom: "15px",
         }}
-        onClick={navigateToPostTutor}
         color="primary"
         aria-label="add"
       >
-        <AddIcon />
+        <AddIcon onClick={navigateToPostTutor} />
       </Fab>
-
-  </div>
-}
+    </div>
+  );
+};
 
 export default TutorPage;
