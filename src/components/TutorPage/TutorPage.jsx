0cacbc69754102d863c7eadc45b99930a574c531
DiffatElicka <903869629@qq.com>
2023-11-30 20:53:35 -0600

    Database Integration & Style fix

--- src/components/TutorPage/TutorPage.jsx
+++ src/components/TutorPage/TutorPage.jsx
@@ -2,27 +2,53 @@ import './Tutorpage.less'
 import TutorCard from "../TutorCard/TutorCard";
 import { useState, useEffect } from "react";
 import SchoolSageHeader from "../SchoolSageHeader/SchoolSageHeader";
+import { useDbData } from "../../utilities/firebaseUtils"
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
 
-
-
+const navigateToPostTutor = () => {
+  navigate("/post-tutor-page")
+};
 
 const TutorPage = () => {
+    const [data, error] = useDbData('/Tutor-Page');
+    if (error) return <div className="tutor-page">
+    <SchoolSageHeader />
 
+    <h1>Error loading course data: {`${error}`}</h1>
+    </div>;
+    if (data === undefined) return <div className="tutor-page">
+    <SchoolSageHeader />
+
+    <h1>Loading data...</h1>
+    </div>;
+    // if (!data) return <h1>No course data found</h1>;
 
     return <div className="tutor-page">
       <SchoolSageHeader />
 
       <div className="posting-box">
-      {[1,2,3,4,5,6,7,8].map(({ item }) => (
-        <TutorCard />
-      ))}
+      {Object.entries(data).map(( itemInfo, id ) => {
+        return <TutorCard key={itemInfo[0]} itemInfo={itemInfo} />}
+        
+      )}
       </div>
 
-    {/* <div className="tutor-card-list">
-      {[1,2,3,4,5,6,7,8].map(({ item }) => (
-        <TutorCard />
-      ))}
-    </div> */}
+      <Fab
+        style={{
+          backgroundColor: "#00008b",
+          color: "white",
+          margin: "0 auto",
+          marginTop: "10px",
+          marginBottom: "15px",
+        }}
+        onClick={navigateToPostTutor}
+        color="primary"
+        aria-label="add"
+      >
+        <AddIcon />
+      </Fab>
+
   </div>
 }
 
