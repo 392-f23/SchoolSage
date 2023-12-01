7827c239bb0207c83013b4b926018e0e8e10dcf9
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-11-30 21:11:22 -0600

    Database Integration & Style fix (#14)
    Co-authored-by: DiffatElicka <903869629@qq.com>

--- src/components/TutorCard/TutorCard.jsx
+++ src/components/TutorCard/TutorCard.jsx
@@ -9,7 +9,6 @@ import "./TutorCard.less";
 import { firebaseSignOut } from "../../utilities/firebaseUtils";
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -18,7 +17,6 @@ import Typography from '@mui/material/Typography';
 import ChatIcon from '@mui/icons-material/Chat';
 import LogoutIcon from '@mui/icons-material/Logout';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import Fab from '@mui/material/Fab';
 
 const categoryColorMapping = {
   "Science": "#2ecc71",    // Green
@@ -28,141 +26,60 @@ const categoryColorMapping = {
   "Writing": "#e67e22"      // Orange
 }; 
 
-const TutorCard = (props) => {
-//   const { itemId, item } = props;
+const TutorCard = ({itemInfo}) => {
+  // const { item } = props;
+  const item = itemInfo[1];
   const navigate = useNavigate();
 
-  const fakeitem = {
-    "uid": "123456",
-    "name": "Chenyu Wang",
-    "location": "Downtown Evanston",
-    "level": "undergrade",
-    "category": ["Science", "Reading"],
-    "time": "Monday to Friday afternoon",
-    "description": "I can teach high school computer science and Reading."
-  }
+  // const item = {
+  //   "uid": "123456",
+  //   "name": "Chenyu Wang",
+  //   "location": "Downtown Evanston",
+  //   "level": "undergrade",
+  //   "category": ["Science", "Reading"],
+  //   "time": "Monday to Friday afternoon",
+  //   "description": "I can teach high school computer science and Reading."
+  // }
 
   const capitalizeFirstLetter = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   };
 
-  return <Card style={{margin:"10px", }} key={fakeitem.uid} className="user-card">
+  return <Card style={{margin:"10px", }} className="user-card">
   <CardContent>
     <Typography variant="h5" component="div">
-                      {fakeitem.name}
-                      {fakeitem.category.map((cate, id) => {
-                        return <div style={{
+                      {item.name}
+                      <div style={{
                           display: 'inline-block',
-                          backgroundColor: categoryColorMapping[cate],
+                          backgroundColor: categoryColorMapping[item.category],
                           color: 'white',
                           padding: '5px 10px',
                           borderRadius: '20px',
                           marginLeft: '10px',
                           fontSize: '0.8rem'
                       }}> 
-                          {cate}
+                          {item.category}
                       </div>
-                      })}
                   </Typography>
     <Typography color="text.secondary">
-      {fakeitem.level}
+      {item.level}
     </Typography>
     <Typography variant="body2">
-      {fakeitem.location}
+      {item.location}
     </Typography>
     <Typography variant="body2">
-      {fakeitem.time}
+      {item.time}
     </Typography>
     <Typography variant="body2">
-      {fakeitem.description}
+      {item.description}
     </Typography>
     
-    <Button style={{  minWidth: 'initial', marginRight: "10px"}} variant="contained" endIcon={<PersonAddIcon />}>Add to Dashboard</Button>
+    <Button style={{  minWidth: 'initial', marginRight: "10px"}} variant="contained" endIcon={<PersonAddIcon />}>I need help</Button>
     <Button  variant="contained" endIcon={<ChatIcon />}>Contact</Button>
   
   </CardContent>
 </Card>
 
-//   return <Card style={{ margin: "10px" }} key={fakeitem.uid} className="user-card">
-//   <CardContent>
-//     <Typography variant="h5" component="div">
-//       {fakeitem.name}
-//     </Typography>
-//     <Typography color="text.secondary">{fakeitem.level}</Typography>
-//             {fakeitem.proficiency.map((prof, id) => {
-//                return <Button
-//               key={id}
-//                size="small"
-//                 className="tutor-card-proficiency-button"
-//                disableRipple={true}
-//                 >
-//                      {prof}
-//                 </Button>
-//                  })
-//             }
-//     <Typography variant="body2">{fakeitem.location}</Typography>
-//     <Typography variant="body2">{fakeitem.time}</Typography>
-//     <Typography variant="body2">{fakeitem.description}</Typography>
-//     <Button variant="contained" endIcon={<ChatIcon />}>
-//       Contact{" "}
-//     </Button>
-//   </CardContent>
-// </Card>
-
-  // return (
-  //   <Card className="tutor-card">
-  //     <CardActionArea>
-  //       <CardContent className="tutor-card-content">
-  //         <Typography
-  //           variant="h5"
-  //           component="div"
-  //           className="tutor-card-name"
-  //         >
-  //           {capitalizeFirstLetter(fakeitem.name)}
-  //         </Typography>
-  //         <Typography
-  //           variant="h6"
-  //           component="div"
-  //           className="tutor-card-location"
-  //         >
-  //           {fakeitem.location}
-  //         </Typography>
-  //         <Typography
-  //           variant="body1"
-  //           component="div"
-  //           className="tutor-card-location"
-  //         >
-  //           {fakeitem.avaliable}
-  //         </Typography>
-  //       </CardContent>
-  //     </CardActionArea>
-  //     <CardActionArea>
-  //     <div className="tutor-card-proficiency">
-  //           {fakeitem.proficiency.map((prof, id) => {
-  //               return <Button
-  //               key={id}
-  //               size="small"
-  //               className="tutor-card-proficiency-button"
-  //               disableRipple={true}
-  //               >
-  //                   {prof}
-  //               </Button>
-  //               })
-  //           }
-  //       </div>
-  //     </CardActionArea>
-  //     <Divider light />
-  //     <CardContent className="tutor-card-content">
-  //         <Typography
-  //           variant="body2"
-  //           color="text.secondary"
-  //           className="tutor-card-description"
-  //         >
-  //           {fakeitem.description}
-  //         </Typography>
-  //       </CardContent>
-  //   </Card>
-  // );
 };
 
 export default TutorCard;
